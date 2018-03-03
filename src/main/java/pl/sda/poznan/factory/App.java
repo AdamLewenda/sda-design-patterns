b2b71a28094065f69727f41378e1664044f2629c
Adam Lewenda <adamlewenda@hotmail.com>
2018-03-03 15:06:17 +0100

    add factory design patterns

--- src/main/java/pl/sda/poznan/factory/App.java
+++ src/main/java/pl/sda/poznan/factory/App.java
@@ -3,15 +3,10 @@ package pl.sda.poznan.factory;
 import pl.sda.poznan.factory.model.Company;
 import pl.sda.poznan.factory.model.Employee;
 import pl.sda.poznan.factory.writer.CompanyWriter;
-import pl.sda.poznan.factory.writer.TxtCompanyWriter;
-import pl.sda.poznan.factory.writer.XmlCompanyWriter;
+
+import java.util.Scanner;
 
 public class App {
-
-    //utworz klase Employee przech dane o pracownikach
-    //.. Imie, nazwisko, email, salary
-    //utworz klase Company
-    //jak zapreprezentowac fakt, ze firma ma pracownikow?
 
     public static void main(String[] args) {
         Employee dyrektor = new Employee("Jan", "Nowak", "nowak@gmail.com", 10000D);
@@ -21,7 +16,16 @@ public class App {
         company.addEmployee(dyrektor);
         company.addEmployee(kierownik);
 
-        CompanyWriter writer = new XmlCompanyWriter("firma.xml");
-        writer.write(company);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj nazwe pliku do ktorego chcesz zapisac");
+
+        String filename = scanner.next();
+        CompanyWriterFactory companyWriterFactory = new CompanyWriterFactory();
+        try {
+            CompanyWriter writer = companyWriterFactory.create(filename);
+            writer.write(company);
+        } catch (IllegalArgumentException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
